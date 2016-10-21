ff8d2c76d3f5974463138da6ad113f5517da186c
Nakshatra Mukhopadhyay <nakshatra@fusioncharts.com>
2016-10-21 20:03:40 +0530

    Mocked out parts of FusionCharts TimeSeries API.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 // import DateRange from './fcts-ext-daterange';
 const DateRange = require('./fcts-ext-daterange');
+const FusionCharts = require('../lib/fusioncharts.js');
+
+var fc = new FusionCharts();
+
+fc.register('extension', ['date-range-chooser', function (id) {
+  var global = this;
+  var extAPI = global.extAPI;
+
+  extAPI({
+    init: function (require) {
+      require('X-Axis', 'Y-Axis', 'lol', function (x, y) {
+        global.x = x;
+        global.y = y;
+      });
+    }
+  });
+}]);
 
 var dateRange = new DateRange();
 console.log(JSON.stringify(dateRange.getRange()));
