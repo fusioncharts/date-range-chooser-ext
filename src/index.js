5b4d45683c382372fe501364ab84aaf61e221d07
Nakshatra Mukhopadhyay <nakshatra@fusioncharts.com>
2016-11-02 13:58:00 +0530

    Updated fusioncharts. Integrated i into code.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ const DateRange = require('./fcts-ext-daterange');
 ;(function (factory) {
   factory(FusionCharts);
 })(function (FC) {
-  FusionCharts.register('extension', ['date-range-chooser', function (id) {
+  FC.register('extension', ['date-range-chooser', function (id) {
     var global = this;
     var extAPI = global.extAPI;
 
-    // var otherAPI = FusionCharts.getExtComponent(id, 'api', 'legacyextapi');
-    // var toolBoxApi = FusionCharts.getComponent('api', 'toolbox');
+    // var otherAPI = FC.getExtComponent(id, 'api', 'legacyextapi');
+    // var toolBoxApi = FC.getComponent('api', 'toolbox');
 
     window.dr = new DateRange();
     extAPI(window.dr);
--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
+var FusionCharts = require('../lib/fusioncharts');
+
 var DateRange = require('../src/fcts-ext-daterange');
 var DateInput = require('../src/fcts-ext-dateinput');
 var StartDateInput = require('../src/fcts-ext-startdateinput');
@@ -9,9 +11,16 @@ var EndDateInput = require('../src/fcts-ext-enddateinput');
 var Calendar = require('../src/fcts-ext-calendar');
 
 describe('DateRange', function () {
-  var dr = new DateRange();
-  dr.init();
-  dr.range = {
+  var self = this;
+  FusionCharts.register('extension', ['date-range-chooser', function (id) {
+    var global = this;
+    var extAPI = global.extAPI;
+
+    self.dr = new DateRange();
+    extAPI(self.dr);
+  }]);
+  var fc = new FusionCharts(); // eslint-disable-line no-unused-vars
+  self.dr.range = {
     startDate: 12000,
     endDate: 13000
   };
@@ -24,38 +33,38 @@ describe('DateRange', function () {
       expect(new DateRange().range.endDate).to.equal(0);
     });
     it('should have the keys startDate and endDate', function () {
-      expect(dr.range).to.have.all.keys(['startDate', 'endDate']);
+      expect(self.dr.range).to.have.all.keys(['startDate', 'endDate']);
     });
     it('should have the value of the key startDate be at most the value of the key endDate', function () {
-      expect(dr.range.startDate).to.be.at.most(dr.range.endDate);
+      expect(self.dr.range.startDate).to.be.at.most(self.dr.range.endDate);
     });
     it('should be unchanged if startDate is set greater than endDate', function () {
-      dr.range = {
+      self.dr.range = {
         startDate: 17000,
         endDate: 12000
       };
-      expect(dr.range.startDate).to.equal(12000);
-      expect(dr.range.endDate).to.equal(13000);
+      expect(self.dr.range.startDate).to.equal(12000);
+      expect(self.dr.range.endDate).to.equal(13000);
     });
     it('should set the values of startDate and endDate', function () {
-      dr.range = {
+      self.dr.range = {
         startDate: 12125,
         endDate: 18676
       };
-      expect(dr.range.startDate).to.equal(12125);
-      expect(dr.range.endDate).to.equal(18676);
+      expect(self.dr.range.startDate).to.equal(12125);
+      expect(self.dr.range.endDate).to.equal(18676);
     });
   });
   describe('#swapDates', function () {
     it('should swap the dates of the range member', function () {
-      var tempStartDt = dr.range.startDate;
-      var tempEndDt = dr.range.endDate;
-      dr.swapDates();
-      expect(dr.range.startDate).to.equal(tempEndDt);
-      expect(dr.range.endDate).to.equal(tempStartDt);
+      var tempStartDt = self.dr.range.startDate;
+      var tempEndDt = self.dr.range.endDate;
+      self.dr.swapDates();
+      expect(self.dr.range.startDate).to.equal(tempEndDt);
+      expect(self.dr.range.endDate).to.equal(tempStartDt);
     });
   });
-  dr.dispose();
+  self.dr.dispose();
 });
 
 describe('DateInput', function () {
